8ef4f99e5c55c351770233e8c37aa6970f3bbaba
giuliano-mota <giulimp@gmail.com>
2022-12-06 16:41:44 -0300

    Foi criado  cindo algoritmos que calculm as quatro operações aritmeticas e o resto da divisão

--- Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,4 +1,28 @@
-let num =1;
-let num2 = 4;
-let result;
-console.log(result = num+num2);
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+//Adição (a + b)
+//Subtração (a - b)
+//Multiplicação (a * b)
+//Divisão (a / b)
+//Módulo (a % b)
+
+//Adição
+let a = 5,b = 7;
+let adicao;
+console.log("Adição: ",adicao = a + b);
+//Subtração
+a = 10, b = 4;
+let subtracao; 
+console.log("Subtração: ",subtracao = a - b);
+//Multipliocação
+a = 7, b = 8;
+let multiplicação;
+console.log("Multiplicação: ", multiplicação = a * b);
+//Divisão
+a = 12, b = 6;
+let divisao
+console.log("Divisão: ",divisao = a / b);
+//Módulo
+a = 25, b = 5;
+let modulo;
+console.log("Módulo: ", modulo = a % b);
+
