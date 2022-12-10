d690f1678e34050a73e223025e6e0743f3c9b686
giuliano-mota <giulimp@gmail.com>
2022-12-10 19:30:25 -0300

    Essa ativida soma todos os valores contidos no array e imprime  o resultado

--- Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript - Array-e-loop-For/scripr.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript - Array-e-loop-For/scripr.js
@@ -5,3 +5,12 @@ for (index = 0; index < numbers.length; index += 1){
     console.log(numbers[index]);
 
 }
+
+//Some todos os valores contidos no array e imprima o resultado;
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+for (index = 0; index < numbers.length; index += 1){
+    result += numbers[index];
+
+}
+console.log("Soma de todos os elementos",result);
