05ba7abe06a433b520a582d2b3c4eb57d02c9f2d
giuliano-mota <giulimp@gmail.com>
2022-12-10 19:41:49 -0300

    Calcula e imprime a média aritmética dos valores contidos no array

--- Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript - Array-e-loop-For/scripr.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript - Array-e-loop-For/scripr.js
@@ -14,3 +14,14 @@ for (index = 0; index < numbers.length; index += 1){
 
 }
 console.log("Soma de todos os elementos",result);
+
+//Calcule e imprima a média aritmética dos valores contidos no array;
+console.log("-------------------------------");
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let sum = 0;
+for (index = 0; index < numbers.length; index += 1){
+    sum += numbers[index];
+
+}
+let average = sum / numbers.length;
+console.log("Média aritmetica", average);
