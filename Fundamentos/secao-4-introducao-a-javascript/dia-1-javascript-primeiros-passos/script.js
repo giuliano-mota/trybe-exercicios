142bd20ebcda84d9ae8af739001ff810f63a512c
giuliano-mota <giulimp@gmail.com>
2022-12-19 19:03:22 -0300

    Atividade 1 comcluída

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,24 @@
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+//Adição (a + b)
+//Subtração (a - b)
+//Multiplicação (a * b)
+//Divisão (a / b)
+//Módulo (a % b)
+
+//Adição
+const a = 15; 
+const b = 5;
+
+console.log("Soma " + (a + b)); 
+//Subtração 
+console.log("Subtração " + (a - b)); 
+//Multiplicação
+console.log("Multiplicação " + (a * b)); 
+//Divisão
+console.log("Divisão " + (a / b)); 
+//Módulo
+console.log("Módulo " + (a % b)); 
+
+
+
+
