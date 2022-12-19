177799dda6f2551e317e7b186f8eab89f7cdc793
giuliano-mota <giulimp@gmail.com>
2022-12-19 19:29:39 -0300

    Atividade 2 e 3 concluída

--- Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/script.js
@@ -1,3 +1,5 @@
+console.log("Atividade 1 ")
+console.log(" ");
 //Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
 //Adição (a + b)
 //Subtração (a - b)
@@ -19,6 +21,33 @@ console.log("Divisão " + (a / b));
 //Módulo
 console.log("Módulo " + (a % b)); 
 
+console.log(" ");
+console.log("Atividade 2 ");
+//Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
 
+const num1 = 45;
+const num2 = 86;
 
+if (num1 > num2){
+    console.log("num1 " + num1 +   " é maior que num2 " + num2);
+}
+else{
+    console.log("num2 " + num2 + " é maior que num1 " + num1);
+}
 
+console.log(" ");
+console.log("Atividade 3 ");
+//Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três constantes com os valores que serão comparados.
+const numA = 152;
+const numB = 220;
+const numC = 150;
+
+if (numA > numB && numA > numC){
+    console.log("numA " + numA + " é o maior número " );
+}
+else if (numB > numC){
+    console.log("numB " + numB + " é o maior número ");
+}
+else{
+    console.log("numC " + numC + " é o maior número ");
+}
